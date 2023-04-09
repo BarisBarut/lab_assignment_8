6e4e8cfc91d72b145209ded1a59f5ce09478d1a0
BarisBarut <125616721+BarisBarut@users.noreply.github.com>
2023-04-09 19:18:50 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,21 +1,32 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
+void swap(int* a, int* b)
+{
+
+    int temp = *a;
+
+    *a = *b;
+
+    *b = temp;
+}
+
 void heapSort(int arr[], int n)
 {
     for (int i = n / 2 - 1; i >= 0; i--)
- 
+
         heapify(arr, n, i);
- 
+
     for (int i = n - 1; i >= 0; i--) {
- 
+
         swap(&arr[0], &arr[i]);
- 
+
         heapify(arr, i, 0);
     }
 }
@@ -23,26 +34,35 @@ void heapSort(int arr[], int n)
 void heapify(int arr[], int n, int i)
 {
     // Find largest among root, left child and right child
- 
+
     // Initialize largest as root
     int largest = i;
- 
+
     // left = 2*i + 1
     int left = 2 * i + 1;
- 
+
     // right = 2*i + 2
     int right = 2 * i + 2;
- 
+
     // If left child is larger than root
     if (left < n && arr[left] > arr[largest])
- 
+
         largest = left;
- 
+
     // If right child is larger than largest
     // so far
     if (right < n && arr[right] > arr[largest])
- 
+
         largest = right;
+    if (largest != i) {
+
+        swap(&arr[i], &arr[largest]);
+
+        // Recursively heapify the affected
+        // sub-tree
+        heapify(arr, n, largest);
+    }
+}
 
 
 // implement merge sort
@@ -53,25 +73,25 @@ void merge(int pData[], int l,
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
- 
+    extraMemoryAllocated += n2*4 + n1*4;
     // Create temp arrays
     int L[n1], R[n2];
- 
+
     // Copy data to temp arrays
     // L[] and R[]
     for (i = 0; i < n1; i++)
         L[i] = pData[l + i];
     for (j = 0; j < n2; j++)
         R[j] = pData[m + 1 + j];
- 
+
     // Merge the temp arrays back
     // into arr[l..r]
     // Initial index of first subarray
     i = 0;
- 
+
     // Initial index of second subarray
     j = 0;
- 
+
     // Initial index of merged subarray
     k = l;
     while (i < n1 && j < n2)
@@ -88,7 +108,7 @@ void merge(int pData[], int l,
         }
         k++;
     }
- 
+
     // Copy the remaining elements
     // of L[], if there are any
     while (i < n1) {
@@ -96,7 +116,7 @@ void merge(int pData[], int l,
         i++;
         k++;
     }
- 
+
     // Copy the remaining elements of
     // R[], if there are any
     while (j < n2)
@@ -111,10 +131,10 @@ void mergeSort(int pData[], int l, int r)
     if (l < r)
     {
         int m = l + (r - l) / 2;
- 
+
         mergeSort(pData, l, m);
         mergeSort(pData, m + 1, r);
- 
+
         merge(pData, l, m, r);
     }
 }
@@ -126,7 +146,7 @@ int parseData(char *inputFileName, int **ppData)
 	int dataSz = 0;
 	int i, n, *data;
 	*ppData = NULL;
-	
+
 	if (inFile)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
@@ -146,13 +166,22 @@ int parseData(char *inputFileName, int **ppData)
 
 		fclose(inFile);
 	}
-	
+
 	return dataSz;
 }
 
 // prints first and last 100 items in the data array
 void printArray(int pData[], int dataSz)
 {
+    if (dataSz < 100)
+    {
+        for (int i = 0; i < dataSz; i++)
+        {
+            printf("%d ",pData[i]);
+        }
+        printf("\n\t");
+        return;
+    }
 	int i, sz = dataSz - 100;
 	printf("\tData:\n\t");
 	for (i=0;i<100;++i)
@@ -160,7 +189,7 @@ void printArray(int pData[], int dataSz)
 		printf("%d ",pData[i]);
 	}
 	printf("\n\t");
-	
+
 	for (i=sz;i<dataSz;++i)
 	{
 		printf("%d ",pData[i]);
@@ -174,21 +203,21 @@ int main(void)
 	int i;
     double cpu_time_used;
 	char* fileNames[] = { "input1.txt", "input2.txt", "input3.txt", "input4.txt" };
-	
+
 	for (i=0;i<4;++i)
 	{
 		int *pDataSrc, *pDataCopy;
 		int dataSz = parseData(fileNames[i], &pDataSrc);
-		
+
 		if (dataSz <= 0)
 			continue;
-		
+
 		pDataCopy = (int *)malloc(sizeof(int)*dataSz);
-	
+
 		printf("---------------------------\n");
 		printf("Dataset Size : %d\n",dataSz);
 		printf("---------------------------\n");
-		
+
 		printf("Heap Sort:\n");
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
@@ -199,7 +228,7 @@ int main(void)
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		
+
 		printf("Merge Sort:\n");
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
@@ -210,9 +239,11 @@ int main(void)
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
-		
+
 		free(pDataCopy);
 		free(pDataSrc);
 	}
-	
 }
+
+
+
