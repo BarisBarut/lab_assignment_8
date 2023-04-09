ffb8bf70615a4de7c056d3b6cc85fe3a732f1169
BarisBarut <125616721+BarisBarut@users.noreply.github.com>
2023-04-09 19:01:39 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -8,13 +8,115 @@ int extraMemoryAllocated;
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+    for (int i = n / 2 - 1; i >= 0; i--)
+ 
+        heapify(arr, n, i);
+ 
+    for (int i = n - 1; i >= 0; i--) {
+ 
+        swap(&arr[0], &arr[i]);
+ 
+        heapify(arr, i, 0);
+    }
 }
+
+void heapify(int arr[], int n, int i)
+{
+    // Find largest among root, left child and right child
+ 
+    // Initialize largest as root
+    int largest = i;
+ 
+    // left = 2*i + 1
+    int left = 2 * i + 1;
+ 
+    // right = 2*i + 2
+    int right = 2 * i + 2;
+ 
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+ 
+        largest = left;
+ 
+    // If right child is larger than largest
+    // so far
+    if (right < n && arr[right] > arr[largest])
+ 
+        largest = right;
 
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l,
+           int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+ 
+    // Copy data to temp arrays
+    // L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = pData[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = pData[m + 1 + j];
+ 
+    // Merge the temp arrays back
+    // into arr[l..r]
+    // Initial index of first subarray
+    i = 0;
+ 
+    // Initial index of second subarray
+    j = 0;
+ 
+    // Initial index of merged subarray
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            pData[k] = L[i];
+            i++;
+        }
+        else
+        {
+            pData[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements
+    // of L[], if there are any
+    while (i < n1) {
+        pData[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2)
+    {
+        pData[k] = R[j];
+        j++;
+        k++;
+    }
+}
 void mergeSort(int pData[], int l, int r)
 {
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+ 
+        mergeSort(pData, l, m);
+        mergeSort(pData, m + 1, r);
+ 
+        merge(pData, l, m, r);
+    }
 }
 
 // parses input file to an integer array
